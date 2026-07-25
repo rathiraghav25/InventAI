b2724902800784f3c2b61f2e20c83b082f8f4fdc
Raghav Rathi <rathiraghav752@gmail.com>
2026-07-25 22:22:09 +0530

    Setup FastAPI backend and PostgreSQL connection

---
+++ backend/app/db/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from app.core.config import settings
+
+
+engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+import app.models.base
+from app.db.database import create_tables
+
+app = FastAPI(title="InventAI API")
+
+
+@app.on_event("startup")
+def startup():
+    create_tables()
+
 
 @app.get("/")
-def home():
-    return {"message": "Welcome to InventAI Backend 🚀"}
+def root():
+    return {"message": "InventAI Backend Running 🚀"}
